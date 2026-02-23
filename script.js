20ca57e75d36e4a6040ec145c750207893487e5c
CSEAIML-1 <dhruvsharma19101@gmail.com>
2026-02-23 15:28:53 +0530

    commit

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 // console.log("Hello World!");
 // document.write("Hello World!");
 
-var num = 10;
-console.log(num);
-console.log(typeof num);
+// var num = 10;
+// console.log(num);
+// console.log(typeof num);
 
-num = false;
-console.log(num);
-console.log(typeof num);
+// num = false;
+// console.log(num);
+// console.log(typeof num);
 
-num = "Sanjeev Soni";
-console.log(num);
-console.log(typeof num);
+// num = "Sanjeev Soni";
+// console.log(num);
+// console.log(typeof num);
 
-num = null;
-console.log(num);
-console.log(typeof num);
+// num = null;
+// console.log(num);
+// console.log(typeof num);
 
-num = undefined;
-console.log(num);
-console.log(typeof num);
+// num = undefined;
+// console.log(num);
+// console.log(typeof num);
+
+// document.write("(a==b)=>");
+// result = (a == b);
+// document.write(result);
+// document.write("linebreak");
+
+// document.write("(a < b)=>");
+// result = (a < b);
+// document.write(result);
+// document.write("linebreak");
+
+// document.write("(a > b)=>");
+// result = (a > b);
+// document.write(result);
+// document.write("linebreak");
+
+var count;
+document.write("Starting Loop" + "<br>");
+for(count = 0; count < 10; count++){
+    document.write("current count" + count);
+    document.write("<br>");
+}
+document.write("Loop Ended");
