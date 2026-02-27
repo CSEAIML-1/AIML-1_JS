690bd112fac4997497d630d36cc976bc2522423d
CSEAIML-1 <dhruvsharma19101@gmail.com>
2026-02-27 09:40:15 +0530

    update

--- script.js
+++ script.js
@@ -44,11 +44,78 @@
 // }
 // document.write("Loop Ended");
 
-const person = {
-    fname: "ajay",
-    lname: "Singh",
-    age: 30,
-};
-for(let x in person){
-    console.log("person details:" + x + ":" + person[x]);
+// const person = {
+//     fname: "ajay",
+//     lname: "Singh",
+//     age: 30,
+// };
+// for(let x in person){
+//     console.log("person details:" + x + ":" + person[x]);
+// }
+
+// var i;
+// for(i = 0; i < 3; i++){
+//     console.log(i);
+// }
+
+// for (key in object){
+//     //code block to be executed
+// }
+// const person = {
+//     fname: "ajay",
+//     lname: "Singh",
+//     age: 30,
+// };
+// for(let x in person){
+//     console.log("person details:" + x + ":" + person[x]);
+// }
+
+// var count = 0;
+// document.write("Starting Loop");
+
+// while(count < 10){
+//     document.write("current count" + count + "<br>");
+//     count++;
+// }
+// document.write("Loop stopped");
+
+// var age = 20;
+// if(age > 18){
+//     console.log("You are eligible to vote");
+// }
+
+// var age = 20;
+// if(age >= 18){
+//     console.log("You are an adult");
+// }else{
+//     console.log("You are a minor");
+// }
+
+
+// var grade = 'A';
+// switch(grade){
+//     case 'A':document.write("Good Job<br/>");
+//     break;
+
+//     case 'B':document.write("Pretty Good<br/>");
+//     break;
+
+//     case 'C':document.write("Passed<br/>");
+//     break;  
+
+//     case 'D':document.write("Not so good<br/>");
+//     break;
+
+//     case 'F':document.write("Failed<br/>");
+//     break;
+
+//     default:document.write("Unknown grade<br/>");
+// }
+
+// function myFunction(){
+//     alert("Hello World!");
+// }
+
+function sayHello(name, age){
+    document.write(name + " is " + age + " years old.");
 }
